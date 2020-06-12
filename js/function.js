4d1ee0980b964c1d5f96354b5ed702a1dc1d6779
Christopher Henderson <chris.henderson.ky@gmail.com>
2020-06-12 10:31:36 -0400

    Edith in progress
    working on  interface

---
+++ js/function.js
@@ -0,0 +1,21 @@
+/*################################
+#########DATE AND TIME############
+################################*/
+
+function showTime() {
+// get a new date (locale machine date time)
+var date = new Date();
+// get the date as a string
+var n = date.toDateString();
+// get the time as a string
+var time = date.toLocaleTimeString();
+
+// find the html element with the id of time
+// set the innerHTML of that element to the date a space the time
+document.getElementById('date').innerHTML = time;
+document.getElementById('clock').innerHTML = n;
+
+setTimeout(showTime, 1000);
+}
+
+showTime();
---
+++ js/weather.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const { temperature, summary } = data.currently;
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = Math.floor(temperature);
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+            })
+                
+            });
+       
+    }
+    
+});
